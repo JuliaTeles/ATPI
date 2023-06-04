f1aeae384a095bad4f12250a45af7822bd86453e
Júlia Téles <julitc@yahoo.com>
2023-06-04 20:00:36 -0300

    Terminar ex de Máximos Locais

--- Lista Arrays Avançados/Ex3 - Máximos Locais.cpp
+++ Lista Arrays Avançados/Ex3 - Máximos Locais.cpp
@@ -5,18 +5,20 @@ using namespace std;
 int main(){
 
     setlocale(LC_ALL, "Portuguese");
-    int quantidade = 0;
+    int quantidade;
     int qtdmaximos = 0;
+
     cout << "Digite a quantidade de números do array: \n";
     cin >> quantidade;
     int numeros[quantidade];
     int maximos[quantidade];
 
-    for(int i=0; i<quantidade; i++){
-        cout << "Digite o número da posição " << i+1 << ":\n";
-        cin >> numeros[i];
+    for(int k=0; k<quantidade; k++){
+        cout << "Digite o número da posição " << k+1 << ":\n";
+        cin >> numeros[k];
     }
 
+    cout << "---------\n";
     for(int i=0; i<quantidade; i++){
         if(i=0){
             if(numeros[i]>numeros[i+1]){
@@ -31,11 +33,14 @@ int main(){
         }else if(numeros[i]>numeros[i-1] && numeros[i]>numeros[i+1]){
             maximos[i] = numeros[i];
             qtdmaximos = i;
+        }else{
+            cout << "Não é máx \n";
         }
     }
 
+    cout << "Qtd maximos: \n" << qtdmaximos << endl;
     cout << "Máximos Locais: \n";
     for(int j=0; j<qtdmaximos; j++){
-        cout << maximos[j];
+        cout << maximos[j] << " | ";
     }
 }
