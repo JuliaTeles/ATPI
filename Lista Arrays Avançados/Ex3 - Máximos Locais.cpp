97198e4bc9bcd07f0b4a88e79c728bb037715533
Júlia Téles <julitc@yahoo.com>
2023-06-07 15:00:32 -0300

    Arrumando códigos

--- Lista Arrays Avançados/Ex3 - Máximos Locais.cpp
+++ Lista Arrays Avançados/Ex3 - Máximos Locais.cpp
@@ -2,45 +2,51 @@
 #include <locale>
 using namespace std;
 
-int main(){
+int main() {
 
-    setlocale(LC_ALL, "Portuguese");
-    int quantidade;
-    int qtdmaximos = 0;
+  setlocale(LC_ALL, "Portuguese");
+  int quantidade;
+  int qtdmaximos = 0;
 
-    cout << "Digite a quantidade de números do array: \n";
-    cin >> quantidade;
-    int numeros[quantidade];
-    int maximos[quantidade];
+  cout << "Digite a quantidade de números do array: \n";
+  cin >> quantidade;
+  int numeros[quantidade];
+  int maximos[quantidade];
 
-    for(int k=0; k<quantidade; k++){
-        cout << "Digite o número da posição " << k+1 << ":\n";
-        cin >> numeros[k];
+  for (int i = 0; i < quantidade; i++) {
+    maximos[i] = 777;
+  }
+
+  for (int k = 0; k < quantidade; k++) {
+    cout << "Digite o número da posição " << k + 1 << ":\n";
+    cin >> numeros[k];
+  }
+
+  cout << "---------\n";
+  for (int i = 0; i < quantidade; i++) {
+    if (i == 0) {
+      if (numeros[i] > numeros[i + 1]) {
+        maximos[i] = numeros[i];
+        qtdmaximos = i;
+      }
+    } else if (i == (quantidade - 1)) {
+      if (numeros[i] > numeros[i - 1]) {
+        maximos[i] = numeros[i];
+        qtdmaximos = i;
+      }
+    } else if (numeros[i] > numeros[i - 1] && numeros[i] > numeros[i + 1]) {
+      maximos[i] = numeros[i];
+      qtdmaximos = i;
+    } else {
+      cout << "Não é máx \n";
     }
+  }
 
-    cout << "---------\n";
-    for(int i=0; i<quantidade; i++){
-        if(i=0){
-            if(numeros[i]>numeros[i+1]){
-                maximos[i] = numeros[i];
-                qtdmaximos = i;
-            }
-        }else if(i = quantidade-1){
-            if(numeros[i]>numeros[i-1]){
-                maximos[i] = numeros[i];
-                qtdmaximos = i;
-            }
-        }else if(numeros[i]>numeros[i-1] && numeros[i]>numeros[i+1]){
-            maximos[i] = numeros[i];
-            qtdmaximos = i;
-        }else{
-            cout << "Não é máx \n";
-        }
+  cout << "Quantidade de máximos: \n" << qtdmaximos << endl;
+  cout << "Máximos Locais: \n";
+  for (int j = 0; j < quantidade; j++) {
+    if (maximos[j] != 777) {
+      cout << maximos[j] << " | ";
     }
-
-    cout << "Qtd maximos: \n" << qtdmaximos << endl;
-    cout << "Máximos Locais: \n";
-    for(int j=0; j<qtdmaximos; j++){
-        cout << maximos[j] << " | ";
-    }
+  }
 }
